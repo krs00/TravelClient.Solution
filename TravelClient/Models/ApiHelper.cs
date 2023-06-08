0c246928078c164d88e44d8002f726b93f3f8358
Kymani Stephens <kymanist00@gmail.com>
2023-06-08 14:54:58 -0700

    update method name

--- TravelClient/Models/ApiHelper.cs
+++ TravelClient/Models/ApiHelper.cs
@@ -5,11 +5,13 @@ namespace TravelClient.Models
 {
     public class ApiHelper
     {
-        public static async Task<string> GetAll()
+        public static async Task<string> GetAllCitiesAsync()
         {
             RestClient client = new RestClient("http://localhost:5000/");
             RestRequest request = new RestRequest($"api/cities", Method.Get);
             RestResponse response = await client.GetAsync(request);
+
+
             return response.Content;
         }
     }
